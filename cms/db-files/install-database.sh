e74012b58a8c50bc55bc33158f7c9f132b612c5c
Norbert Madauß <norbert.madauss@delphi-imm.de>
2025-07-18 12:35:40 +0200

    update directus & edit install script

--- cms/db-files/install-database.sh
+++ cms/db-files/install-database.sh
@@ -25,28 +25,6 @@ psql $PGOPTIONS $CONNECTIONSTRING/postgres < "$DBINIT/02_database-users.sql"
 echo "$(date -u) SQL - Create DB extensions $CONNECTIONSTRING"
 psql $PGOPTIONS $CONNECTIONSTRING/$APPDB < "$DBINIT/03_database-extensions.sql"
 
-# CLEAN INSTALL ---------
-#echo "$(date -u) SQL - Start importing $APPFOLDER/clean/app-schema.sql in $CONNECTIONSTRING/$APPDB"
-#psql $PGOPTIONS $CONNECTIONSTRING/$APPDB < "$APPFOLDER/clean/app-schema.sql"
-#echo "$(date -u) SQL - Finished importing $APPFOLDER/clean/app-schema.sql in $CONNECTIONSTRING/$APPDB"
-
-#echo "$(date -u) SQL - Start importing $APPFOLDER/clean/app-data.sql in $CONNECTIONSTRING/$APPDB"
-#psql $PGOPTIONS $CONNECTIONSTRING/$APPDB < "$APPFOLDER/clean/app-data.sql"
-#echo "$(date -u) SQL - Finished importing $APPFOLDER/aclean/pp-data.sql in $CONNECTIONSTRING/$APPDB"
-# --------------------------------
-
-echo "$(date -u) SQL - Start importing $APPFOLDER/app-schema.sql in $CONNECTIONSTRING/$APPDB"
-psql $PGOPTIONS $CONNECTIONSTRING/$APPDB < "$APPFOLDER/app-schema.sql"
-echo "$(date -u) SQL - Finished importing $APPFOLDER/app-schema.sql in $CONNECTIONSTRING/$APPDB"
-
-echo "$(date -u) SQL - Start importing $APPFOLDER/app-data.sql in $CONNECTIONSTRING/$APPDB"
-psql $PGOPTIONS $CONNECTIONSTRING/$APPDB < "$APPFOLDER/app-data.sql"
-echo "$(date -u) SQL - Finished importing $APPFOLDER/app-data.sql in $CONNECTIONSTRING/$APPDB"
-
-echo "$(date -u) SQL - Start importing $USERDATAFOLDER/user-schema.sql in $CONNECTIONSTRING/$APPDB"
-psql $PGOPTIONS $CONNECTIONSTRING/$APPDB < "$USERDATAFOLDER/user-schema.sql"
-echo "$(date -u) SQL - Finished importing $USERDATAFOLDER/user-schema.sql in $CONNECTIONSTRING/$APPDB"
-
-echo "$(date -u) SQL - Start importing $USERDATAFOLDER/user-data.sql in $CONNECTIONSTRING/$APPDB"
-psql $PGOPTIONS $CONNECTIONSTRING/$APPDB < "$USERDATAFOLDER/user-data.sql"
-echo "$(date -u) SQL - Finished importing $USERDATAFOLDER/user-data.sql in $CONNECTIONSTRING/$APPDB"
+echo "$(date -u) SQL - Start importing complete_db.sql in $CONNECTIONSTRING/$APPDB"
+psql $PGOPTIONS $CONNECTIONSTRING/$APPDB < "complete_db.sql"
+echo "$(date -u) SQL - Finished importing importing complete_db.sql in $CONNECTIONSTRING/$APPDB"
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 Get started
 
-text
+## Update Directus
+
+1. `cd cms/directus`
+2. `npm update`
+3. `npm install directus`
+4. `npm install pg`
+5. `npx directus database migrate:latest`
